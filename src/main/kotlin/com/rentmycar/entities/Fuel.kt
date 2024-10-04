bd4ded7b15f36de942a3c60239dac0a4d5eb2b2b
Dany <97673955+dan-898@users.noreply.github.com>
2024-10-04 21:01:32 +0200

    kan-24 Create the database entities in the project

--- src/main/kotlin/com/rentmycar/entities/Fuel.kt
+++ src/main/kotlin/com/rentmycar/entities/Fuel.kt
@@ -6,12 +6,12 @@ import org.jetbrains.exposed.dao.id.EntityID
 
 
 object Fuels : IntIdTable() {
-    val name = varchar("name", 50) // Name of the fuel type (e.g., Gasoline, Diesel, Electric)
+    val name = varchar("name", 50)
 }
 
 
 class Fuel(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<Fuel>(Fuels)
 
-    var name by Fuels.name // Fuel type name
+    var name by Fuels.name
 }
