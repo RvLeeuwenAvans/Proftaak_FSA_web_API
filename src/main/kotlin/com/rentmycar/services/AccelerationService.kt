f7357abf14bffe85d80717765a1db5e4abb1d15a
RvLeeuwen <r.vanleeuwen2@student.avans.nl>
2024-10-23 21:24:38 +0200

    fix validation

--- src/main/kotlin/com/rentmycar/services/AccelerationService.kt
+++ src/main/kotlin/com/rentmycar/services/AccelerationService.kt
@@ -4,10 +4,15 @@ import kotlin.math.sqrt
 
 class AccelerationService {
     fun calculateVelocity(initialVelocity: Double, acceleration: Double, deltaTime: Double): Double {
+        // speed + acceleration / time acceleration
         return initialVelocity + acceleration * deltaTime
     }
 
     fun calculateAccelerationMagnitude(ax: Double, ay: Double, az: Double): Double {
+        //https://www.xylenepower.com/Vector%20Identities.htm
+        //Ax = Vector A's magnitude component parallel to x axis;
+        //Ay = Vector A's magnitude component parallel to y axis;
+        //Az = Vector A's magnitude component parallel to z axis;
         return sqrt(ax * ax + ay * ay + az * az)
     }
 }
