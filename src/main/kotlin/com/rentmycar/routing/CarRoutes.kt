6884ed38c7ede4a03611882ff4fb35ccb283644b
RvLeeuwen <r.vanleeuwen2@student.avans.nl>
2025-01-17 11:31:10 +0100

    changed some routing relating to the reservation logic, to better couple with the front-end. also reworked the directions endpoint, so that we can actually render the directions in app.

--- src/main/kotlin/com/rentmycar/routing/CarRoutes.kt
+++ src/main/kotlin/com/rentmycar/routing/CarRoutes.kt
@@ -3,11 +3,12 @@ package com.rentmycar.routing
 import com.rentmycar.controllers.CarController
 import com.rentmycar.controllers.LocationController
 import io.ktor.server.auth.*
+import io.ktor.server.config.*
 import io.ktor.server.routing.*
 
-fun Route.carRoutes() {
+fun Route.carRoutes(config: ApplicationConfig) {
 
-    val carController = CarController()
+    val carController = CarController(config)
     val locationController = LocationController()
 
     authenticate {
@@ -16,7 +17,7 @@ fun Route.carRoutes() {
             get("/{id}") { carController.getCar(call) }
             get("/all/filtered") { carController.getFilteredCars(call) }
 
-            get("/directions") { carController.getDirectionsToCar(call) }
+            post("/directions") { carController.getDirectionsToCar(call) }
             put("/update") { carController.updateCar(call) }
             delete("/{id}") { carController.deleteCar(call) }
 
